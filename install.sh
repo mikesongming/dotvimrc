e4e1ccaa1d7f0d1463d55581a292ad77588dc30b
root <root@test05.k8s>
2018-07-25 11:07:57 -0400

    fix bug in dot_zshrc

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c PluginInstall
 pushd ~/.vim/bundle/tern_for_vim && npm install && popd
-pushd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer --java-completer --js-completer --go-completer && popd
+#pushd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer --java-completer --js-completer --go-completer && popd
