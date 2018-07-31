47bf06e879ec90c0b702025113c3d178ee3ae722
gnosoir <mikesongming@gmail.com>
2018-07-31 09:27:25 -0400

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-cp -f dot_vimrc ~/.vimrc
 cp -f dot_zshrc ~/.zshrc
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh 
+
+cp -f dot_vimrc ~/.vimrc
 
 mkdir -p ~/.vim/templates
 cp templates/* ~/.vim/templates/
@@ -9,5 +11,6 @@ cp templates/* ~/.vim/templates/
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c PluginInstall
+
 pushd ~/.vim/bundle/tern_for_vim && npm install && popd
 #pushd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer --java-completer --js-completer --go-completer && popd
