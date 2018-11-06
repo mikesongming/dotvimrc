096fd9e73ee822fd20ab443386022765744319db
gnosoir <mikesongming@gmail.com>
2018-11-06 15:42:23 +0800

    add pythonstartup

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
-cp -f dot_zshrc ~/.zshrc
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh 
-
-cp -f dot_tmux.conf ~/.tmux.conf
-
-cp -f dot_vimrc ~/.vimrc
-
 mkdir -p ~/.vim/templates
 cp templates/* ~/.vim/templates/
 
@@ -15,4 +8,13 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c PluginInstall
 
 pushd ~/.vim/bundle/tern_for_vim && npm install && popd
+
+cp -f dot_zshrc ~/.zshrc
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh 
+
+cp -f dot_tmux.conf ~/.tmux.conf
+
+cp -f dot_vimrc ~/.vimrc
+
+cp -f pythonstartup.py ~/.pythonstartup
 #pushd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer --java-completer --js-completer --go-completer && popd
